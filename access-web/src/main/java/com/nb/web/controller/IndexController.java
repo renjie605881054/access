95531a9461342fab3e6c4778523e6079c7a1b6bc
renjie605881054 <renjie@users.noreply.github.com>
2017-12-07 15:51:56 +0800

    增加日志配置

--- access-web/src/main/java/com/nb/web/controller/IndexController.java
+++ access-web/src/main/java/com/nb/web/controller/IndexController.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/index/")
 public class IndexController {
 	
-	@RequestMapping(value="{name}")
+	@RequestMapping(value = "{name}", method = RequestMethod.GET)
 	public String indexData(@PathVariable String name, HttpServletRequest request){
 		return "index";
 	}
--- access-web/src/main/java/com/nb/web/start/WebApplication.java
+++ access-web/src/main/java/com/nb/web/start/WebApplication.java
@@ -2,10 +2,8 @@ package com.nb.web.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.nb.web"})
+@SpringBootApplication(scanBasePackages= {"com.nb.web"})
 public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
